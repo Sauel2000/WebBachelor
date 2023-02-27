df31132090a0e9ab356ec51aacd453679f4e5941
Sauel2000 <samuel.said00@outlook>
2023-02-27 13:42:24 +0100

    Object example

---
+++ Program/camera.py
@@ -0,0 +1,6 @@
+class Cam:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        return name, age
+
--- Program/main.py
+++ Program/main.py
@@ -1,1 +1,14 @@
-print("Hello")
+import camera
+import module
+
+# Created a class object
+object = module.GFG()
+   
+# Calling and printing class methods
+print(object.add(15,5))
+print(object.sub(15,5))
+   
+# Calling the function
+module.method()
+#objectCam = camera("Test", 9)
+#print(objectCam.method())
