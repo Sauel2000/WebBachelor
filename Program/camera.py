133551a95df4b20b14ce160dad4efd9130a7a9cb
Sauel2000 <samuel.said00@outlook>
2023-02-27 17:17:45 +0100

    OpenCV Detecting cirkles

--- Program/camera.py
+++ Program/camera.py
@@ -1,6 +1,16 @@
-class Cam:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        return name, age
+# Python program showing a use
+# of get() and set() method in
+# normal function
 
+class Geek:
+    def __init__(self, age = 0):
+        self._age = age
+      
+    # getter method
+    def get_age(self):
+        return self._age
+      
+    # setter method
+    def set_age(self, x):
+        self._age = x
+        
