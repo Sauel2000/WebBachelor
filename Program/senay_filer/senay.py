b2afbf62d3a66817c2593b32cf24b968fbd0e4fd
Senay A <eggm636@gmail.com>
2023-03-01 15:49:54 +0100

    Did changes on files

--- Program/senay_filer/senay.py
+++ Program/senay_filer/senay.py
@@ -1,5 +1,12 @@
 import cv2
 
+
 path = "blackdot.jpg"
 
+#Loading an image in grayscale
 gray = cv2.imread(path, 0)
+
+#Converts image to binary image, where pixels are set to 0 or 255 
+th, threshed = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV|cv2.THRES_OTSU)
+
+
