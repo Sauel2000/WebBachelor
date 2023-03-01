197f513976bb18871bce17560a55571584a7f93b
Sauel2000 <samuel.said00@outlook>
2023-03-01 21:53:27 +0100

    Structure fixed

--- Program/samuel_filer/sam.py
+++ Program/samuel_filer/sam.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-img = cv.imread('gradient.png',0)
+img = cv.imread('image_thres1.jpg',0)
 ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
 ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
 ret,thresh3 = cv.threshold(img,127,255,cv.THRESH_TRUNC)
