45ab478c3c56e8d8ff3dd682b9c107a7f5c05374
Kristoffer Paulsen <kristoffer.h.h.paulsen@gmail.com>
2023-03-01 09:49:52 +0100

    Endringer

--- Program/bitch.py
+++ Program/bitch.py
@@ -1,3 +1,32 @@
+<<<<<<< Updated upstream
 print("Samuel er en bitch")
 print("It do be fax tho")
 print("Actually true")
+=======
+# import required libraries
+import cv2
+
+# Reading the Image
+image = cv2.imread(r'C:\Users\krist\Senay.jpg')
+
+# initialize the HOG descriptor
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# detect humans in input image
+(humans, _) = hog.detectMultiScale(image, winStride=(10, 10),
+padding=(32, 32), scale=1.1)
+
+# getting no. of human detected
+print('Human Detected : ', len(humans))
+
+# loop over all detected humans
+for (x, y, w, h) in humans:
+   pad_w, pad_h = int(0.15 * w), int(0.01 * h)
+   cv2.rectangle(image, (x + pad_w, y + pad_h), (x + w - pad_w, y + h - pad_h), (0, 255, 0), 2)
+
+# display the output image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+>>>>>>> Stashed changes
