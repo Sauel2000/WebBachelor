48b07693b4e88925e71f74ec15b0cba02e07d846
Sauel2000 <samuel.said00@outlook>
2023-03-02 09:36:40 +0100

    deleted cp files

--- Program/samuel_filer/main.py
+++ Program/samuel_filer/main.py
@@ -1,12 +1,20 @@
-import numpy as np
-import sys
-import cv2 as cv
-img = cv.imread('skyteskive.jpg',0)
-img = cv.medianBlur(img,5)
-cimg = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
+import sys # to access the system
+import cv2
+import os
+path = 'C:\\Users\\Samue\\Desktop\\BachelorGit\\WebBachelor\\Program\\samuel_filer\\samuel_bilder\\skyteskive.jpg'
 
+
+img = cv2.imread(path, cv2.IMREAD_ANYCOLOR)
  
+while True:
+    cv2.imshow("skive", img)
+    cv2.waitKey(0)
+    sys.exit() # to exit from all the processes
+ 
+cv2.destroyAllWindows() # destroy all windows
 
+
+'''
 circles = cv.HoughCircles(img,cv.HOUGH_GRADIENT,1,20,
                             param1=50,param2=30,minRadius=0,maxRadius=0)
 circles = np.uint16(np.around(circles))
@@ -18,3 +26,4 @@ for i in circles[0,:]:
 cv.imshow('detected circles',cimg)
 cv.waitKey(0)
 cv.destroyAllWindows()
+'''
