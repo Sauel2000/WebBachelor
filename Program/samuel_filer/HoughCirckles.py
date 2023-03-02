f7a806f7c7b5098edc3400800f488a7f3c4f3e12
Sauel2000 <samuel.said00@outlook>
2023-03-02 11:52:45 +0100

    Updated working code

--- Program/samuel_filer/HoughCirckles.py
+++ Program/samuel_filer/HoughCirckles.py
@@ -1,20 +1,13 @@
 import sys # to access the system
-import cv2
-import os
-path = 'C:\\Users\\Samue\\Desktop\\BachelorGit\\WebBachelor\\Program\\samuel_filer\\samuel_bilder\\skyteskive.jpg'
+import cv2 as cv
+import numpy as np
+path = "C:/Users/Samue/Desktop/BachelorGit/WebBachelor/Program/samuel_filer/samuel_bilder/skyteskive.jpg"
 
 
-img = cv2.imread(path, cv2.IMREAD_ANYCOLOR)
- 
-while True:
-    cv2.imshow("skive", img)
-    cv2.waitKey(0)
-    sys.exit() # to exit from all the processes
- 
-cv2.destroyAllWindows() # destroy all windows
 
-
-'''
+img = cv.imread(r'path',0)
+img = cv.medianBlur(img,5)
+cimg = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
 circles = cv.HoughCircles(img,cv.HOUGH_GRADIENT,1,20,
                             param1=50,param2=30,minRadius=0,maxRadius=0)
 circles = np.uint16(np.around(circles))
@@ -26,4 +19,3 @@ for i in circles[0,:]:
 cv.imshow('detected circles',cimg)
 cv.waitKey(0)
 cv.destroyAllWindows()
-'''
