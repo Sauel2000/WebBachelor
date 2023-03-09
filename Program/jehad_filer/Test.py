d8eae999ebf539bf303c3fab6d44da18cf2e1f54
Jehad Baba <jehad3306@gmail.com>
2023-03-09 15:48:55 +0100

    På Test.py kan jeg nå finne treffpunkter som er market manuelt rød farge

--- Program/jehad_filer/Test.py
+++ Program/jehad_filer/Test.py
@@ -6,7 +6,8 @@ start_time = time.time()
 
 PATH = "C:/Users/jehad/Desktop/WebBachelor/Program/samuel_filer/samuel_bilder/skyteSkive.jpg"
 PATH2 = "C:/Users/jehad\Desktop/WebBachelor/Program/jehad_filer/solutions_try_out/color_image.jpg"
-img = cv2.imread(PATH)
+PATH3 = "C:/Users/jehad/Desktop/WebBachelor/Program/jehad_filer/solutions_try_out/skyteskive_ThreeMark.jpg"
+img = cv2.imread(PATH3)
 
 width = 1280
 height = 720
@@ -14,38 +15,75 @@ dim = (width, height)
 
 wall = False
 
-x_img = 10
-y_img = 10
+x_img = 2
+y_img = 2
 
-x1 = 10
-y1 = 0
-x2 = 20
-y2 = 10
+
 row = 1
+
+
 
 resized_img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
 while(wall == False):
+    x1 = x_img - 2 # -10
+    y1 = y_img - 2 # -10
+
+    x2 = x_img
+    y2 = y_img
     pixel_center = resized_img[(y_img,x_img)] # dobbel sjekk om x og y ligger i riktig parameter posisjon
     #cv2.rectangle(resized_img, (x1,y1), (x2,y2), (0, 255, 0), 1)
-    print(pixel_center)
-    #print("image shape:", resized_img.shape)
+    #print(pixel_center)
+
+    '''
+    if(pixel_center[0] < 100):
+         x_img += 5
+         pixel_center = resized_img[(y_img,x_img)]
+
+         if(pixel_center[0] > 180):
+              x_img += 5
+              pixel_center = resized_img[(y_img,x_img)]
+              if(pixel_center[0] < 100):
+                    cv2.rectangle(resized_img, (x1,y2), (x2,y2), (0, 255, 0), 2)
+                    print("\n --------------------------------------- \n\n\n STOP DU FANT DEN STOP STOP TOP!! \n\n\n ----------------------------- \n")
+                    wall = True
+              x_img -= 5
+         x_img -= 5
+     '''
+    
+    if(( pixel_center[0] < 10) and  pixel_center[1] < 36 and (pixel_center[2] > 200 and pixel_center[2] < 256 )):
+             
+          cv2.rectangle(resized_img, (x1,y1), (x2,y2), (0, 255, 0), 1)
+          print(x1,y1,x2,y2)
+          #print(x_img, y_img)
+          #print("DONE!")
+          
+    '''
+    if(pixel_center[0] > 200):
+         
+         print("done, run :",pixel_center)
+         x_img += 10
+         pixel_center = resized_img[(y_img,x_img)]
+         print(pixel_center)
+         wall = True
+    '''
     if (x_img == 1270):
-        y_img += 10
-        x_img = 10
-        print("going to row 2 | y position = ", y_img,  "\n\n")
+        y_img += 2
+        x_img = 2
+        #print("going to row", row ,"| y position = ", y_img,  "\n\n")
+        row += 1
+
+
 
     if (y_img == 710):
             wall = True
-    x_img += 10
-    print("x position: ", x_img)
-    #x1 += 10
-    #x2 += 10
-    '''
-    cv2.imshow('detect holes',resized_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    '''
+    x_img += 2
+    #print("x position: ", x_img)
+    
+cv2.imshow('detect holes',resized_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+    
 print("\n Process finished in:", np.ceil((time.time()- start_time)),"seconds")
 
 
--- Program/jehad_filer/jehad.py
+++ Program/jehad_filer/jehad.py
@@ -1,27 +1,24 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('C:/Users/jehad/Desktop/WebBachelor/Program/samuel_filer/samuel_bilder/skyteSkive.jpg')
+img = cv2.imread("C:/Users/jehad/Desktop/WebBachelor/Program/samuel_filer/samuel_bilder/SkyteSkive.jpg")
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lower_bound = np.array[(50, 20, 20)]
-upper_bound = np.array[(100, 255, 255)]
-mask = cv2.inRange(hsv, lower_bound, upper_bound)
+pixel_center = img[(160,650)]
 
-#define kernel size  
-kernel = np.ones((7,7),np.uint8)
+print(pixel_center)
+print(pixel_center[2])
+width = 1280
+height = 720
+dim = (width, height)
 
-# Remove unnecessary noise from mask
+resized_img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+resized_img[(160,650)] = [0,255,0]
+resized_img[(160,655)] = [0,255,0]
 
-mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+#cv2.rectangle(resized_img, (650,160), (660,170), (0, 255, 0), 1)
 
-segmented_img = cv2.bitwise_and(img, img, mask=mask)
 
-contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-output = cv2.drawContours(segmented_img, contours, -1, (0, 0, 255), 3)
-
-cv2.imshow('detect holes', output)
+cv2.imshow('detect holes', resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
