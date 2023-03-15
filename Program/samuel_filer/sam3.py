40f56b7c827bb0b7247641bf6168046f969eb89c
Senay A <eggm636@gmail.com>
2023-03-15 15:55:26 +0100

    Doxygen documentation added

--- Program/samuel_filer/sam3.py
+++ Program/samuel_filer/sam3.py
@@ -1,7 +1,7 @@
 
 '''
 
-General CV2 library
+##General CV2 library
 
 '''
 import cv2 as cv
@@ -18,17 +18,31 @@ Cv2 Section
 https://docs.opencv.org/3.4/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
 
 '''
+
+## Documentation for a function
+# Function that reads the input image
 img = cv.imread(path1)
 
-
-#Colors are all in BGR format
+# Color we search for, red in this case.
 SearchColor = (255, 0, 0)
+
+# Color of the the circle to be drawn, blue in this case. 
 colorCircle = (0,0,255)
+
+# Variable representing the size of the radius of the circle
 CircleMarkingRadius = 10
+
 window_name = "Chil"
+
+# Thickness of the circle
 thickness=1
+
+# A list to store the coordinates of the shots
 shotCoords = []
+
+# Value of each shot
 shotValue = 0
+
 #Check every pixel in each row to match the searching color
 for y in range(img.shape[0]):
     for x in range(img.shape[1]):
